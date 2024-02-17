0b771ea8b53263e9465fb91dade189e36a21bf4f
wwweert123 <puachengxuan.fp@gmail.com>
2024-02-17 17:00:02 +0800

    connect front end onclick event of buttons with backend using axios client

--- src/App.js
+++ src/App.js
@@ -5,11 +5,76 @@ import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import { useState } from "react";
 
+import axios from "./api/axios";
+
+const ADD_ENPOINT = "/plus-minus/add";
+const SUBTRACT_ENDPOINT = "/plus-minus/subtract";
+
 function App() {
     const [number1, setNumber1] = useState();
     const [number2, setNumber2] = useState();
 
     const [result, setResult] = useState();
+
+    const SubmitAdd = async () => {
+        const params = new URLSearchParams({
+            number1: number1,
+            number2: number2,
+        });
+        try {
+            const response = await axios.post(ADD_ENPOINT, params, {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            });
+            const result = response?.data?.answer;
+            if (!isNaN(result)) {
+                setResult(result);
+            } else {
+                alert("non-number received from backend");
+            }
+        } catch (err) {
+            console.log(err);
+            if (!err?.response) {
+                alert("No Server Response");
+            } else if (err.response?.status === 400) {
+                alert(
+                    "Incorrect parameters sent: " + err.response?.data?.message
+                );
+            } else if (err.response?.status === 404) {
+                alert("API endpoint not found");
+            } else {
+                alert("Unexpected error");
+            }
+        }
+    };
+
+    const SubmitSubtract = async () => {
+        const params = new URLSearchParams({
+            number1: number1,
+            number2: number2,
+        });
+        try {
+            const response = await axios.post(SUBTRACT_ENDPOINT, params, {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            });
+            const result = response?.data?.answer;
+            if (!isNaN(result)) {
+                setResult(result);
+            }
+        } catch (err) {
+            console.log(err);
+            if (!err?.response) {
+                console.log("No Server Response");
+            } else if (err.response?.status === 400) {
+                console.log("Incorrect parameters sent");
+            } else {
+                console.log("Unexpected error");
+            }
+        }
+    };
 
     return (
         <Stack
@@ -40,13 +105,18 @@ function App() {
             </Stack>
 
             <Stack spacing={2} direction="row">
-                <Button variant="contained" endIcon={<AddIcon />}>
+                <Button
+                    variant="contained"
+                    endIcon={<AddIcon />}
+                    onClick={SubmitAdd}
+                >
                     Add
                 </Button>
                 <Button
                     variant="contained"
                     color="warning"
                     endIcon={<RemoveIcon />}
+                    onClick={SubmitSubtract}
                 >
                     Subtract
                 </Button>
