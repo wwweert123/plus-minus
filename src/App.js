2e7fad38e2462cfb69da2e050bc9d6f7f9833d72
wwweert123 <puachengxuan.fp@gmail.com>
2024-02-17 14:30:04 +0800

    Add textfield for 2 numbers, add button for addition and subtraction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 // import "./App.css";
 
-import { Typography } from "@mui/material";
+import { Typography, Stack, TextField, Button } from "@mui/material";
 
 function App() {
-    return <Typography variant="h2">Plus and Minus</Typography>;
+    return (
+        <Stack
+            spacing={5}
+            justifyContent="center"
+            alignItems="center"
+            display="flex"
+            mt={2}
+        >
+            <Typography variant="h2">Plus and Minus</Typography>
+            <Stack spacing={2} direction="row">
+                <TextField
+                    id="number1"
+                    label="First number"
+                    variant="outlined"
+                />
+                <TextField
+                    id="number2"
+                    label="Second number"
+                    variant="outlined"
+                />
+            </Stack>
+
+            <Stack spacing={2} direction="row">
+                <Button variant="contained">Add</Button>
+                <Button variant="contained">Subtract</Button>
+            </Stack>
+        </Stack>
+    );
 }
 
 export default App;
