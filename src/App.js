4216cb3bdc2e05d4323465b822b39cea52787c60
wwweert123 <puachengxuan.fp@gmail.com>
2024-02-17 18:18:58 +0800

    add default value for number1 and number2

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
 
     const SubmitAdd = async () => {
         const params = new URLSearchParams({
-            number1: number1,
-            number2: number2,
+            number1: number1 ? number1 : 0,
+            number2: number2 ? number2 : 0,
         });
         try {
             const response = await axios.post(ADD_ENPOINT, params, {
