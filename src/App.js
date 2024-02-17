a64575c7577207d5eab34baff8c71b4e57e130bc
wwweert123 <puachengxuan.fp@gmail.com>
2024-02-17 14:38:56 +0800

    use react useState hook to store state of the numbers

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@
 import { Typography, Stack, TextField, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { useState } from "react";
 
 function App() {
+    const [number1, setNumber1] = useState();
+    const [number2, setNumber2] = useState();
+
+    const [result, setResult] = useState();
+
     return (
         <Stack
             spacing={5}
@@ -20,12 +26,14 @@ function App() {
                     label="First number"
                     variant="outlined"
                     type="number"
+                    value={number1}
                 />
                 <TextField
                     id="number2"
                     label="Second number"
                     variant="outlined"
                     type="number"
+                    value={number2}
                 />
             </Stack>
 
@@ -41,7 +49,7 @@ function App() {
                     Subtract
                 </Button>
             </Stack>
-            <Typography variant="h5">Calculated Result: </Typography>
+            <Typography variant="h5">Calculated Result: {result}</Typography>
         </Stack>
     );
 }
